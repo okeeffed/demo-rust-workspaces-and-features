c4bdcb8e38ec40fe9a760aa35aa6720b0a60bfd8
Dennis O'Keeffe <hello@dennisokeeffe.com>
2024-11-12 09:00:06 +1100

    fix: ignore workspace target output

--- two/target/debug/deps/two-3e75d662255a5da1.d
+++
@@ -1,5 +0,0 @@
-/Users/dennis.okeeffe/code/scratchpad/workspaces/two/target/debug/deps/libtwo-3e75d662255a5da1.rmeta: src/lib.rs
-
-/Users/dennis.okeeffe/code/scratchpad/workspaces/two/target/debug/deps/two-3e75d662255a5da1.d: src/lib.rs
-
-src/lib.rs:
--- two/target/debug/deps/two-a856d314c88d5028.d
+++
@@ -1,5 +0,0 @@
-/Users/dennis.okeeffe/code/scratchpad/workspaces/two/target/debug/deps/libtwo-a856d314c88d5028.rmeta: src/lib.rs
-
-/Users/dennis.okeeffe/code/scratchpad/workspaces/two/target/debug/deps/two-a856d314c88d5028.d: src/lib.rs
-
-src/lib.rs:
